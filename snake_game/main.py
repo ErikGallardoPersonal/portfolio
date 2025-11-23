56e845c119e5c0d24f8ea83441738262f8980ab5
Erik <erik.gallardo.pro@gmail.com>
2025-11-23 11:01:28 -0600

    refactor(snake): New class Snake
    Main function now will know less about movement and snake specifics

--- snake_game/main.py
+++ snake_game/main.py
@@ -1,10 +1,8 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 from time import sleep
+from snake import Snake
 WINDOW_WIDTH_PIX = 600
 WINDOW_HEIGHT_PIX = WINDOW_WIDTH_PIX
-TURTLE_WIDTH_PIX = 20
-TURTLE_HEIGHT_PIX = 20
-
 
 def main():
     screen = Screen()
@@ -13,28 +11,12 @@ def main():
     screen.title("Ouroboros")
     screen.tracer(0)
 
-    snake_segments: list[Turtle] = []
-    segment_position = [0, 0]
-    for _ in range(3):
-        snake_segment = Turtle(shape="square")
-        snake_segment.penup()
-        snake_segment.color("white")
-        snake_segment.goto(segment_position[0], segment_position[1])
-        snake_segments.append(snake_segment)
-        segment_position[0] -= TURTLE_WIDTH_PIX
-
+    snake = Snake()
     is_game_running = True
     while is_game_running:
         screen.update()
         sleep(0.1)
-        snake_size = len(snake_segments)
-        next_segment_pos_pix = [0, 0]
-        for seg_idx in range(snake_size-1, 0, -1):
-            current_segment = snake_segments[seg_idx]
-            next_segment = snake_segments[seg_idx-1]
-            next_segment_pos_pix = [*next_segment.pos()]
-            current_segment.goto(next_segment_pos_pix[0], next_segment_pos_pix[1])
-        snake_segments[0].forward(TURTLE_WIDTH_PIX)
+        snake.move()
 
     screen.exitonclick()
 
---
+++ snake_game/snake.py
@@ -0,0 +1,26 @@
+from turtle import Turtle
+
+class Snake:
+    def __init__(self) -> None:
+        self._segments: list[Turtle] = []
+        self._segment_size = 20
+        self._init_segments()
+
+    def _init_segments(self):
+        segment_position = [0, 0]
+        for _ in range(3):
+            snake_segment = Turtle(shape="square")
+            snake_segment.penup()
+            snake_segment.color("white")
+            snake_segment.goto(segment_position[0], segment_position[1])
+            self._segments.append(snake_segment)
+            segment_position[0] -= self._segment_size
+
+    def move(self):
+        next_segment_pos_pix = [0, 0]
+        for seg_idx in range(len(self._segments)-1, 0, -1):
+            current_segment = self._segments[seg_idx]
+            next_segment = self._segments[seg_idx-1]
+            next_segment_pos_pix = [*next_segment.pos()]
+            current_segment.goto(next_segment_pos_pix[0], next_segment_pos_pix[1])
+        self._segments[0].forward(self._segment_size)
