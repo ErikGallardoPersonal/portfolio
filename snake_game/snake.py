c6493a6de7bbcd748253cfdb1aa7ae3f6dbc93c7
Erik <erik.gallardo.pro@gmail.com>
2025-11-28 17:56:27 -0600

    Refactor: Create snake from bottom to head
    Make it faster to add new segments to the snake as append is O(1) while insert is O(n)

--- snake_game/snake.py
+++ snake_game/snake.py
@@ -2,6 +2,7 @@ from turtle import Turtle
 from enum import IntEnum
 from constants import GRID_SIZE_PIX
 
+
 class TurnDegrees(IntEnum):
     RIGHT = 0
     UP = 90
@@ -9,17 +10,18 @@ class TurnDegrees(IntEnum):
     DOWN = 270
     FULL = 360
 
+
 class Snake:
     def __init__(self) -> None:
         self.segment_size_pix = GRID_SIZE_PIX
         self._collision_margin = 2
-        self._segments: list[Turtle] = []
+        self.segments: list[Turtle] = []
         self._init_segments()
         self.can_turn = True
-    
+
     @property
     def head(self) -> Turtle:
-        return self._segments[-1]
+        return self.segments[-1]
 
     def move(self) -> None:
         self._segment_propagation()
@@ -30,7 +32,7 @@ class Snake:
         if not self.can_turn:
             return
         if not self._is_valid_movement(current_direction=self.head.heading(),
-                                        desired_direction=desired_direction):
+                                       desired_direction=desired_direction):
             return
         self.head.setheading(desired_direction)
         self.can_turn = False
@@ -49,14 +51,15 @@ class Snake:
 
     def ate(self, food: Turtle) -> bool:
         return self.head.distance(food) < self.segment_size_pix//2 + GRID_SIZE_PIX//2 - self._collision_margin
-    
+
     def collided_with_wall(self, x_limit_pix: int, y_limit_pix: int) -> bool:
         margin = self.segment_size_pix // 2 + self._collision_margin
-        return  (self.head.xcor() < -x_limit_pix + margin or self.head.xcor() > x_limit_pix - margin) or\
-                (self.head.ycor() < -y_limit_pix + margin or self.head.ycor() > y_limit_pix - margin)
+        return (self.head.xcor() < -x_limit_pix + margin or self.head.xcor() > x_limit_pix - margin) or\
+            (self.head.ycor() < -y_limit_pix +
+             margin or self.head.ycor() > y_limit_pix - margin)
 
     def collided_with_tail(self) -> bool:
-        for current_segment in self._segments[:-1]:
+        for current_segment in self.segments[:-1]:
             if self.head.distance(current_segment) < self.segment_size_pix - self._collision_margin:
                 return True
         return False
@@ -67,27 +70,28 @@ class Snake:
         head_heading = self.head.heading()
         self.head.backward(self.segment_size_pix)
         snake_segment = self._create_segment(seg_pos_x_pix=head_position[0],
-                                                seg_pos_y_pix=head_position[1],
-                                                direction=head_heading,
-                                                segment_size_ratio=self.segment_size_pix/20)
-        self._segments.append(snake_segment)
+                                             seg_pos_y_pix=head_position[1],
+                                             direction=head_heading,
+                                             segment_size_ratio=self.segment_size_pix/20)
+        self.segments.append(snake_segment)
 
     def _init_segments(self) -> None:
         INITIAL_SEGMENTS = 3
-        seg_x_pix = (INITIAL_SEGMENTS - 1) * self.segment_size_pix
+        OFFSET = 3
+        seg_x_pix = (INITIAL_SEGMENTS - OFFSET - 1) * self.segment_size_pix
         seg_y_pix = 0
         for _ in range(INITIAL_SEGMENTS):
             snake_segment = self._create_segment(seg_pos_x_pix=seg_x_pix,
                                                  seg_pos_y_pix=seg_y_pix,
                                                  direction=TurnDegrees.RIGHT,
                                                  segment_size_ratio=self.segment_size_pix/20)
-            self._segments.append(snake_segment)
+            self.segments.append(snake_segment)
             seg_x_pix += self.segment_size_pix
 
     def _segment_propagation(self) -> None:
-        for seg_idx in range(0, len(self._segments)-1):
-            current_segment = self._segments[seg_idx]
-            next_segment = self._segments[seg_idx+1]
+        for seg_idx in range(0, len(self.segments)-1):
+            current_segment = self.segments[seg_idx]
+            next_segment = self.segments[seg_idx+1]
             current_segment.goto(next_segment.pos())
 
     @staticmethod
@@ -96,18 +100,19 @@ class Snake:
         snake_segment.penup()
         snake_segment.color("white")
         snake_segment.setheading(direction)
-        snake_segment.shapesize(stretch_len=segment_size_ratio, stretch_wid=segment_size_ratio)
+        snake_segment.shapesize(
+            stretch_len=segment_size_ratio, stretch_wid=segment_size_ratio)
         snake_segment.goto(seg_pos_x_pix, seg_pos_y_pix)
         return snake_segment
-    
+
     @staticmethod
     def _is_valid_movement(current_direction: TurnDegrees, desired_direction: TurnDegrees) -> bool:
         contrary_direction = Snake._get_contrary_direction(current_direction)
-        return not(desired_direction == current_direction or desired_direction == contrary_direction)
-        
+        return not (desired_direction == current_direction or desired_direction == contrary_direction)
+
     @staticmethod
     def _get_contrary_direction(direction: TurnDegrees) -> int:
         inverse_direction = direction + TurnDegrees.FULL // 2
-        if inverse_direction >= TurnDegrees.FULL: # limit direction to positive values between 0 and 270
+        if inverse_direction >= TurnDegrees.FULL:  # limit direction to positive values between 0 and 270
             inverse_direction -= TurnDegrees.FULL
         return inverse_direction
